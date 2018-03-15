ce7c4b46f129588d6d046ca9e94f3f23ca70b64c
Paul <bernier.pja@gmail.com>
2018-03-14 22:30:43 -0700

    Add factoid transaction metrics

--- src/factoid-metrics.js
+++ src/factoid-metrics.js
@@ -4,8 +4,39 @@ const factoidTransactionsGauge = new Gauge({
     name: 'factoid_transactions_nb',
     help: 'Number of factoid transactions in the last block'
 });
+const totalFactoshisTransferedGauge = new Gauge({
+    name: 'factoshis_transfered',
+    help: 'Number of factoshis transfered in the last block'
+});
+const totalFactoshisUsedToBuyECGauge = new Gauge({
+    name: 'factoshis_buy_ec',
+    help: 'Number of factoshis used to buy ECs in the last block'
+});
+const totalECsBoughtGauge = new Gauge({
+    name: 'ec_bought_nb',
+    help: 'Number of ECs bought in the last block'
+});
+const totalFeesPaidGauge = new Gauge({
+    name: 'fees_paid',
+    help: 'Number of factoshis paid as transaction fees in the last block'
+});
 
 exports.computeMetrics = async function(cli, head) {
     const fb = await cli.getFactoidBlock(head.getFactoidBlockKeymr());
+    const ecRate = fb.entryCreditRate;
+
+    let totalFactoshisTransfered = 0,
+        totalFactoshisSpentOnEc = 0,
+        totalFeesPaid = 0;
+    for (const transaction of fb.transactions) {
+        totalFactoshisTransfered += transaction.totalFactoidOutputs;
+        totalFactoshisSpentOnEc += transaction.totalEntryCreditOutputs;
+        totalFeesPaid += transaction.feesPaid;
+    }
+
     factoidTransactionsGauge.set(fb.transactions.length);
+    totalFactoshisTransferedGauge.set(totalFactoshisTransfered);
+    totalFactoshisUsedToBuyECGauge.set(totalFactoshisSpentOnEc);
+    totalFeesPaidGauge.set(totalFeesPaid);
+    totalECsBoughtGauge.set(ecRate ? totalFactoshisSpentOnEc / ecRate : 0);
 };
